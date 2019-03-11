458987c3518604a476be05e06d209281aa86ee7d
Adam Pacholski <apachols@gmail.com>
2019-03-10 18:01:55 -0700

    Better react state pattern, XHR promises

--- src/App.js
+++ src/App.js
@@ -49,21 +49,37 @@ if (hash) {
   console.log('Auth not ready, no hash');
 }
 
-const xhrGetRequest = function(url, headers, callback) {
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', url);
-  Object.entries(headers).forEach(([key, value]) => {
-      xhr.setRequestHeader(key, value);
-  });
+var makeRequest = function (url, method, headers) {
+	var request = new XMLHttpRequest();
 
-  xhr.onreadystatechange = function() {
-      if (xhr.readyState === 4) {
-          callback(xhr.response);
+	return new Promise(function (resolve, reject) {
+
+		request.onreadystatechange = function () {
+			// Only run if the request is complete
+			if (request.readyState !== 4) {
+        return;
       }
-  }
-  xhr.send(null);
-  return xhr;
-}
+      // Resolve if OK, else reject
+      if (request.status >= 200 && request.status < 300) {
+				resolve(request);
+			} else {
+				reject({
+					status: request.status,
+					statusText: request.statusText
+				});
+			}
+		};
+
+		request.open(method || 'GET', url, true);
+
+    // Add request headers
+    Object.entries(headers).forEach(([key, value]) => {
+      request.setRequestHeader(key, value);
+    });
+
+		request.send();
+	});
+};
 
 const songNameFromSpotifyResponse = (rawResponse) => {
   try {
@@ -95,55 +111,68 @@ const lyricsLinkFromGeniusResponse = (rawResponse) => {
 const getCurrentlyPlaying = () => {
   const spotifyApiUrl = 'https://api.spotify.com/v1/me/player/currently-playing?market=US';
   const headers = {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${window.access_token}`
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${window.access_token}`
   };
-  xhrGetRequest(spotifyApiUrl, headers, (response) => {
-      console.log(response);
-      window.currentlyPlaying = songNameFromSpotifyResponse(response);
-  });
+  return makeRequest(spotifyApiUrl, 'GET', headers)
 }
 
-const getLyricsForSong = (searchQuery) => () => {
+const getLyricsForSong = (searchQuery) => {
   const geniusApiUrl = `https://api.genius.com/search?q=${searchQuery}&access_token=${window.access_token}`;
   const headers = {
       'Accept': 'application/json',
   };
-  xhrGetRequest(geniusApiUrl, headers, (response) => {
-      console.log(response);
-      window.lyricsLink = lyricsLinkFromGeniusResponse(response);
-  });
+  return makeRequest(geniusApiUrl, 'GET', headers);
 }
 
-window.currentlyPlaying = 'Never Gonna Give You Up';
-window.lyricsLink = '/';
-
 class App extends Component {
+  state = {
+    currentlyPlaying: 'Never Gonna Give You Up',
+    lyricsLink: 'https://genius.com/Rick-astley-never-gonna-give-you-up-lyrics'
+  };
+
+  setCurrentlyPlaying = () => {
+    getCurrentlyPlaying().then(({ response }) => {
+      const currentlyPlaying = songNameFromSpotifyResponse(response);
+      this.setState(state => ({ ...state, currentlyPlaying }));
+    });
+  };
+
+  setLyricsLink = () => {
+    const { currentlyPlaying } = this.state;
+    getLyricsForSong(currentlyPlaying).then(({ response }) => {
+      const lyricsLink = lyricsLinkFromGeniusResponse(response);
+      this.setState(state => ({ ...state, lyricsLink }));
+    });
+  };
+
   render() {
+    const {currentlyPlaying, lyricsLink} = this.state;
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1>Geniusify</h1>
             <div>
-                <a href={spotifyAuthUrl}>Log In To Spotify</a>
+              <a class="App-link" href={spotifyAuthUrl}>Log In To Spotify</a>
             </div>
             <div>
-              <a href={geniusAuthUrl}>Log In To Genius</a>
+              <a class="App-link" href={geniusAuthUrl}>Log In To Genius</a>
             </div>
+            <br></br>
             <div>
-                <button onClick={getCurrentlyPlaying} type='button'>
+                <button onClick={this.setCurrentlyPlaying} type='button'>
                   What song is playing?
                 </button>
             </div>
-            <h4>{window.currentlyPlaying}</h4>
+            <h4>{currentlyPlaying}</h4>
             <div>
-                <button onClick={getLyricsForSong(window.currentlyPlaying)} type='button'>
+                <button onClick={this.setLyricsLink} type='button'>
                   What are the lyrics?
                 </button>
             </div>
-            <h4>{window.lyricsLink}</h4>
+            <h4>{lyricsLink}</h4>
         </header>
       </div>
     );
